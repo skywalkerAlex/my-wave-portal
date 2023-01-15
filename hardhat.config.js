94bad67d5fca66ca6894bbe128d271fd2d306772
Alex Liakopoulos <mscaidl-0007@uniwa.gr>
2023-01-15 13:04:17 +0200

    Tutorial completed (#1)
    * Create first contract Completed
    * Tutorial Completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config(); // this is for the .env file
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +18,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports={
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,77 @@
 const main=async () => {
+    const [owner, randomPerson]=await hre.ethers.getSigners();
     const waveContractFactory=await hre.ethers.getContractFactory("WavePortal");
-    const waveContract=await waveContractFactory.deploy();
+    const waveContract=await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    /*
+    * Get Contract balance
+    */
+    let contractBalance=await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    let waveCount=await waveContract.getTotalWaves();
+    console.log("WaveCount: "+waveCount.toNumber());
+
+
+    const firstWaveTxn=await waveContract.wave("First wave");
+    await firstWaveTxn.wait(); // Wait for the transaction to be mined
+
+    waveCount=await waveContract.getTotalWaves();
+    console.log("WaveCount: "+waveCount.toNumber());
+
+    /*
+    * Get Contract balance to see what happened!
+    */
+    contractBalance=await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    const secondWaveTxn=await waveContract.connect(randomPerson).wave("Second wave!");
+    await secondWaveTxn.wait();
+
+    waveCount=await waveContract.getTotalWaves();
+    console.log("WaveCount: "+waveCount.toNumber());
+
+    console.log("remove wave");
+    waveContract.unWave();
+
+    waveCount=await waveContract.getTotalWaves();
+    console.log("WaveCount: "+waveCount.toNumber());
+
+    /*
+    * Get Contract balance to see what happened!
+    */
+    contractBalance=await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    let allWaves=await waveContract.getAllWaves();
+    console.log("AllWaves: "+allWaves);
 };
 
 const runMain=async () => {
     try {
         await main();
-        process.exit(0); // exit Node process without error
+        process.exit(0);
     } catch(error) {
         console.log(error);
-        process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+        process.exit(1);
     }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
 };
 
 runMain();
